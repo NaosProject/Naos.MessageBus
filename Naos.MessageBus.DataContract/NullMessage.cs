3f028830e73cf3d15407c6aa8a8eff750791dcb1
Lawson Caudill <lawson@caudillclan.us>
2015-06-19 16:32:24 -0400

    Added null message and handler, added logic to inject a null message into the sequence when it's recurring.  This will allow the null message handler on the harness to honor the message then immediately re-queue the rest in the correct queues, thus allowing recurring targeted queue messages.

---
+++ Naos.MessageBus.DataContract/NullMessage.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullMessage.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.MessageBus.DataContract
+{
+    /// <summary>
+    /// Fake message that doesn't do anything.
+    /// </summary>
+    public class NullMessage : IMessage
+    {
+        /// <inheritdoc />
+        public string Description { get; set; }
+    }
+}
---
+++ Naos.MessageBus.Hangfire.Harness/NullMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullMessageHandler.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.MessageBus.Hangfire.Harness
+{
+    using Naos.MessageBus.DataContract;
+    using Naos.MessageBus.HandlingContract;
+
+    /// <summary>
+    /// No implementation handler to handle NullMessages.
+    /// </summary>
+    public class NullMessageHandler : IHandleMessages<NullMessage>
+    {
+        /// <inheritdoc />
+        public void Handle(NullMessage message)
+        {
+            // no-op...
+        }
+    }
+}
