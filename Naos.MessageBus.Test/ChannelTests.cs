eb9b0976b097a958a252b8f1a16c09829ea7ab51
Lawson Caudill <lawson@caudillclan.us>
2015-08-23 11:06:54 -0400

    Add test to prove out issue with calling Distinct() on a collection of channels.

---
+++ Naos.MessageBus.Test/ChannelTests.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ChannelTests.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.MessageBus.Test
+{
+    using System.Linq;
+
+    using Naos.MessageBus.DataContract;
+
+    using Xunit;
+
+    public class ChannelTests
+    {
+        [Fact]
+        public void DisinctOnChannelCollection_Duplicates_ReturnsActualDistinct()
+        {
+            var duplicateName = "HelloDolly";
+            var input = new[] { new Channel { Name = duplicateName }, new Channel { Name = duplicateName } };
+            var actual = input.Distinct().ToList();
+            Assert.Equal(1, actual.Count);
+            Assert.Equal(duplicateName, actual.Single().Name);
+        }
+    }
+}
