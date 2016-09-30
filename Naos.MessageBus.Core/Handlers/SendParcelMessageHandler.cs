4ea83df048e4f82116af4feaee51c5d6ee020319
Lawson Caudill <lawson@caudillclan.us>
2016-09-30 19:31:32 -0400

    Added sendparcel message and handler and tests.  This allows a message to be queued up and then stacked with a wait and/or retry message afterwards simply by config...

---
+++ Naos.MessageBus.Core/Handlers/SendParcelMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SendParcelMessageHandler.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.MessageBus.Core
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Naos.MessageBus.Domain;
+
+    /// <inheritdoc />
+    public class SendParcelMessageHandler : IHandleMessages<SendParcelMessage>, IShareTrackingCodes
+    {
+        /// <inheritdoc />
+        public async Task HandleAsync(SendParcelMessage message)
+        {
+            if (message.ParcelToSend == null)
+            {
+                throw new ArgumentException("No parcel provided to send.");
+            }
+
+            var postOffice = HandlerToolShed.GetPostOffice();
+            await this.HandleAsync(message, postOffice);
+        }
+
+        /// <summary>
+        /// Handles a <see cref="SendParcelMessage"/>.
+        /// </summary>
+        /// <param name="message">Message to handle.</param>
+        /// <param name="postOffice">Post office to send the message.</param>
+        /// <returns>Task for async.</returns>
+        public async Task HandleAsync(SendParcelMessage message, IPostOffice postOffice)
+        {
+            var trackingCode = postOffice.Send(message.ParcelToSend);
+
+            this.TrackingCodes = new[] { trackingCode };
+
+            await Task.Run(() => { });
+        }
+
+        /// <inheritdoc />
+        public TrackingCode[] TrackingCodes { get; set; }
+    }
+}
---
+++ Naos.MessageBus.Domain/Messages/SendParcelMessage.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SendParcelMessage.cs" company="Naos">
+//   Copyright 2015 Naos
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.MessageBus.Domain
+{
+    /// <summary>
+    /// Message to send a parcel and share the tracking code.
+    /// </summary>
+    public class SendParcelMessage : IMessage
+    {
+        /// <inheritdoc />
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parcel to be sent.
+        /// </summary>
+        public Parcel ParcelToSend { get; set; }
+    }
+}
