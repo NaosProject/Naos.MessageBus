ebb35324888b711c2f76d5d4b1a9ec9fdf43845f
wlscaudill <lawson@caudillclan.us>
2018-10-09 21:55:45 -0300

    Moved ExampleMessage and ExampleMessageHandler into HandlerFactory.Common and referenced DiscoverHandlersInAssemblies directl.y by HanderlFactory. class to keep the example passive.

--- Naos.MessageBus.Hangfire.Console/HandlerFactory.Example.cs.pp
+++ Naos.MessageBus.Hangfire.Console/HandlerFactory.Example.cs.pp
@@ -15,13 +15,8 @@ namespace $rootnamespace$
 {
     using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
-
-    using Its.Log.Instrumentation;
 
     using Naos.MessageBus.Domain;
-
-    using static System.FormattableString;
 
     /// <summary>
     /// Factory builder to provide logic to resolve the appropriate <see cref="IHandleMessages" /> for a dispatched <see cref="IMessage" /> implementation.
@@ -41,36 +36,6 @@ namespace $rootnamespace$
         /// Map of the message type to the intended handler type.  Must have a parameterless constructor and implement <see cref="IHandleMessages" />,
         /// however deriving from <see cref="MessageHandlerBase{T}" /> is recommended as it's more straightforward and easier to write.
         /// </summary>
-        private static readonly IReadOnlyDictionary<Type, Type> MessageTypeToHandlerTypeMap = DiscoverHandlersInAssemblies(new[] { typeof(ExampleMessage).Assembly, typeof(ExampleMessageHandler).Assembly, });
+        private static readonly IReadOnlyDictionary<Type, Type> MessageTypeToHandlerTypeMap = HandlerFactory.DiscoverHandlersInAssemblies(new[] { typeof(ExampleMessage).Assembly, typeof(ExampleMessageHandler).Assembly, });
     }
-
-#if NaosMessageBusHangfireConsole
-    /// <summary>
-    /// Example of an <see cref="IMessage" />.
-    /// </summary>
-    public class ExampleMessage : IMessage
-    {
-        /// <inheritdoc cref="IMessage" />
-        public string Description { get; set; }
-
-        /// <summary>
-        /// Gets or sets an example of a group of work to process.
-        /// </summary>
-        public string GroupToProcess { get; set; }
-    }
-
-    /// <summary>
-    /// Handler for <see cref="ExampleMessage" />.
-    /// </summary>
-    public class ExampleMessageHandler : MessageHandlerBase<ExampleMessage>
-    {
-        /// <inheritdoc cref="MessageHandlerBase{T}" />
-        public override async Task HandleAsync(ExampleMessage message)
-        {
-            await Task.Run(() => { });
-
-            Log.Write(() => Invariant($"Finished processing group: {message.GroupToProcess}"));
-        }
-    }
-#endif
 }
